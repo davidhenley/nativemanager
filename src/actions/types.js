f69816128935e5d40fcf981f4ba40cffb9da2fa6
davidhenley <henleydr@gmail.com>
2017-02-17 12:47:44 -0600

    Employee Actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,9 @@
+// Authentication
 export const EMAIL_CHANGED = 'EMAIL_CHANGED';
 export const PASSWORD_CHANGED = 'PASSWORD_CHANGED';
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
 export const LOGIN_USER_FAIL = 'LOGIN_USER_FAIL';
 export const IS_LOADING = 'LOADING';
+
+// EmployeeActions
+export const EMPLOYEE_UPDATE = 'EMPLOYEE_UPDATE';

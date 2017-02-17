f35f3e367e2f075a5d0fdc4b2e59c23a7474a14d
davidhenley <henleydr@gmail.com>
2017-02-17 12:06:48 -0600

    EmployeeCreate wireup

---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  Container,
+  Content,
+  Text
+} from 'native-base';
+
+class EmployeeCreate extends Component {
+  render() {
+    return (
+      <Container>
+        <Content>
+          <Text>Employee Form</Text>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+export default EmployeeCreate;
--- src/router.js
+++ src/router.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { Router, Scene } from 'react-native-router-flux';
+import { Router, Scene, Actions } from 'react-native-router-flux';
 
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+
+import {
+  Icon
+} from 'native-base';
 
 const RouterComponent = () => {
   const { scene } = styles;
   return (
     <Router sceneStyle={scene}>
-      <Scene key="auth">
-        <Scene key="login" title="Please Login" component={LoginForm} />
+      <Scene initial key="auth">
+        <Scene initial key="login" title="Please Login" component={LoginForm} />
       </Scene>
       <Scene key="main">
-        <Scene key="employeeList" title="Employees" component={EmployeeList} />
+        <Scene
+          initial
+          key="employeeList"
+          title="Employees"
+          rightTitle={<Icon name="add" />}
+          onRight={() => Actions.employeeCreate()}
+          component={EmployeeList} />
+        <Scene
+          key="employeeCreate"
+          title="Create Employee"
+          component={EmployeeCreate} />
       </Scene>
     </Router>
   );
